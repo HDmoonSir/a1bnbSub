ce4e27b254d97bdd4b5279387a334d3692fa9cee
movie5 <oyh5800@naver.com>
2023-07-21 18:57:00 +0900

    delete alll

--- django_web-master_tw/homepage/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import mainpage
-
-# Register your models here.
-admin.site.register(mainpage)
--- react-app/router-tutorial/src/data/utils.js
+++
@@ -1,8 +0,0 @@
-export function getImageUrl(person) {
-    return (
-      'https://i.imgur.com/' +
-      person.imageId +
-      's.jpg'
-    );
-  }
-  
