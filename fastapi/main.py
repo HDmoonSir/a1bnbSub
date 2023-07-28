3dec2d0cc3316d0b6a9232c270a11efa43338189
kimsj00 <tmdwo8814@gmail.com>
2023-07-28 13:57:16 +0900

    'fastapi모델output확인'

--- fastapi/main.py
+++ fastapi/main.py
@@ -8,6 +8,16 @@ from fastapi.responses import Response
 from starlette.middleware.cors import CORSMiddleware
 from typing import List
 
+import os
+import json
+
+
+
+# related detection module
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+import ultralytics
+from ultralytics import YOLO
+
 app =FastAPI()
 
 origins= ["*"]
@@ -20,31 +30,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# from pymongo import MongoClient
-# client =MongoClient('localhost', 27017)
-# db= client.kdt
 
-# # db.users.insert_one({'name': 'admin'}) # insert
-# db.users.delete_one({'name':'admin'})
 
-# all_users= list(db.users.find({}))
-# print(all_users)
+# @app.post('/upload')
+# async def upload_images(images: List[UploadFile] = File(...)):
+#     print("come!!!!!!!!!!!!!!!!!")
+    
+#     # file_data = images[0].file.read()
+#     file_data = images[0].file.read()
+#     image = Image.open(io.BytesIO(file_data))
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+#     model = YOLO('detection.pt')
+#     result = model(image)
+#     print(result[0].tojson())
+#     # print(result.tojson())
+#     return {}
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+def custom_jsonify(result, file_names):
+    output = {}
 
-# django '/upload' post 요청시 호출
-# 모델로 이미지를 넘겨주는 부분 
-@app.post("/upload")
+    for idx, lst in enumerate(result):
+        n = file_names[idx]
+        output[n] = {}
+        for item in lst:
+            ite = json.loads(item.tojson())
+            name = ite[0]["name"]
+            bbox = [ite[0]["box"]["x1"], ite[0]["box"]["y1"], ite[0]["box"]["x2"], ite[0]["box"]["y2"], ite[0]["confidence"]]
+            output[n][name] = bbox
+    return output
+
+
+
+
+
+@app.post('/upload')
 async def upload_images(images: List[UploadFile] = File(...)):
-    # 여기에서 이미지를 처리하는 로직을 구현합니다.
-    # 예를 들면 이미지를 저장하거나 분석하는 작업 등이 가능합니다.
-    file_data = images[0].file.read()
-    pil_image = Image.open(io.BytesIO(file_data))
-    pil_image = pil_image.resize((224, 224))
+    file_data = [image.file.read() for image in images]
+    infer_images = [Image.open(io.BytesIO(data)) for data in file_data]
+
+    file_names = [image.filename for image in images]
+
+    model = YOLO('detection.pt')
+    result = model(infer_images)
+    
+    print(custom_jsonify(result, file_names))
     return {}
+
+
