c48d591597ad82b2c9db217840a03d70e77a3c16
sejeongak <csj971031@gmail.com>
2023-08-05 15:37:35 +0900

    header a1bnb 버튼 활성화

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () =>{
         {/* fixed="top"  */}
           <Navbar bg="primary" data-bs-theme="dark">
             <Container>
-              <Navbar.Brand href="">A1BnB</Navbar.Brand>
+              <Navbar.Brand href="/">A1BnB</Navbar.Brand>
               <Nav className="ml-auto">
                 <Nav.Link href="/user/regist">호스트가 되어 보세요</Nav.Link> 
                 <Nav.Link href="/">Home</Nav.Link> 
