2874c979a8548063a84cdad8e24562343d044741
MoonHD <heedong262@gmail.com>
2023-08-06 15:00:48 +0900

    🐛delete react things on manage.py for db

--- backend/manage.py
+++ backend/manage.py
@@ -16,22 +16,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
-    # add
-    # react 부분 제거 필요
-    try: # 리액트 
-        if sys.argv[2] == 'react':
-            react_root = Path(__file__).resolve().parent.parent / 'frontend'
-            project_root= os.getcwd()
-            os.system("npm run build")
-            # os.system("yarn build")
-            os.chdir(project_root)
-            sys.argv.pop(2)
-    except IndexError:
-        execute_from_command_line(sys.argv)
-    else:
-        execute_from_command_line(sys.argv)
-
-
+        
 if __name__ == '__main__':
     main()
