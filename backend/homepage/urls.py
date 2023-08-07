1dbde85f8e2644793be3137406290b35d1db89a8
mcjeong95 <mcjeong@postech.ac.kr>
2023-08-07 17:02:28 +0900

    feat: backend API 수정

--- backend/homepage/urls.py
+++ backend/homepage/urls.py
@@ -1,24 +1,12 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
-from .views import get_homepage, upload_images, get_rooms, get_mypage, get_result, get_uploaded_page
-
-router = DefaultRouter()
-router.register('posts', views.PostViewSet)
-router.register('images', views.PostViewSet)
+from django.urls import path
+from .views import get_homepage, upload_images, get_room, get_mypage, get_result, upload_post, get_uploaded_page
 
 urlpatterns =[
-    path('home/', get_homepage), 
+    path('/', get_homepage), 
     path('user/', get_mypage),
     path('user/regist', upload_images),
     path('user/regist/result/', get_result),
+    path('user/regist/result/', upload_post),
     path('user/regist/uploaded/', get_uploaded_page),
-    path('room/', get_rooms),
-
-    # 게시물 api
-    path('api/', include(router.urls)),
+    path('room/', get_room),
 ]
-
-# 오류 발생 시 확인!
-#urlpatterns = format_suffix_patterns(urlpatterns)
