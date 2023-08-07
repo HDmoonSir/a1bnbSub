923dbffb929f7e27ca7a679b5b746555394f27eb
sejeongak <csj971031@gmail.com>
2023-08-08 03:46:49 +0900

    마이페이지 구현

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,10 +1,55 @@
 // 마이 페이지 
-const User = () => {
+import { useSearchParams, useEffect, useState } from 'react';
+import { useNavigate, useLocation} from "react-router-dom";
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { back_ip_port } from './back_ip_port';
+
+const serverUrl = `${back_ip_port}`;
+function User() {
+    const navigate = useNavigate();
+    const [imageList, setImageList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            // const postid = searchParams.get('postid');
+            const response = await axios.get(`${serverUrl}/user`);
+            const userData = response.data; // user 데이터 배열
+
+            setImageList(userData); // 이미지 리스트에 최신 데이터 할당
+        } catch (error) {
+            console.error('Error fetching images:', error);
+        }
+  };
+  const handleCardClick = (postId) => {
+      navigate(`/room?postid=${postId}`);
+};
   return (
-    <div>
-      <h1 style={{textAlign:'center'}}>마이 페이지</h1>
-      <p style={{textAlign:'center'}}>마이 페이지 화면입니다</p>
-    </div>
+      <div>
+        <h1 style={{textAlign:'center'}}>마이 페이지</h1>
+        <p style={{textAlign:'center'}}>마이 페이지 화면입니다</p>
+        <Row sx={1} md={4} className="g-4">
+            {imageList.map((info, idx) => (
+                <Col key={idx}>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={info.thumbnail} />
+                    <Card.Body>
+                        <Card.Title>{info.title}</Card.Title>
+                        <Button variant="primary" onClick={() => handleCardClick(info.postId)}>이동</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+        ))}
+        </Row>
+
+      </div>
   );
 };
 
