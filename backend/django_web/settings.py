657fe426ed64c439ac392e11ecae4ae49efb8c49
mcjeong95 <mcjeong@postech.ac.kr>
2023-08-07 18:12:00 +0900

    fix: fix urls in urls.py

--- backend/django_web/settings.py
+++ backend/django_web/settings.py
@@ -188,13 +188,13 @@ STATICFILES_DIRS = [] # 리액트 템플릿
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated",],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-    ],
-}
+# REST_FRAMEWORK = {
+#     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated",],
+#     "DEFAULT_AUTHENTICATION_CLASSES": [
+#         "rest_framework_simplejwt.authentication.JWTAuthentication",
+#         "rest_framework.authentication.SessionAuthentication",
+#     ],
+# }
 
 JWT_AUTH = {
     "JWT_SECRET_KEY": secret_key,  # FIXME: JWT_SECRET_KEY
