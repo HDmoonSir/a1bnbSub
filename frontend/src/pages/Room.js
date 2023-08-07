cbe194f307ffe7d630174e9f18ab0fd7b5cb2ade
MoonHD <heedong262@gmail.com>
2023-08-07 16:20:23 +0900

    ✨made room page proto

--- frontend/src/pages/Room.js
+++ frontend/src/pages/Room.js
@@ -1,25 +1,132 @@
 // 메인에서 숙소 열람 페이지 
 import Image from 'react-bootstrap/Image';
+// useSearchParams로 전 페이지에서 보내온 post_id값을 받아옴
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { back_ip_port } from './back_ip_port'
 
-function ImageExample() {
-  return <Image src="https://tensorflow.org/images/bedroom_hrnet_tutorial.jpg" fluid />;
-}
+const serverUrl = `${back_ip_port}room`;
+
+const othersImageView = (roomInfo) => {
+  // list로 만들어서 출력한다? ㄴㄴ
+  // 리빙룸1
+  // 사진 사이즈로 열거 (브라우저 넓이 / 사진갯수로 줄여서)
+  // 어메니티 출력
+  let showRoomsByClassification = JSON.stringify(roomInfo);
+  showRoomsByClassification = JSON.parse(showRoomsByClassification);
+  // Object.values(showRoomsByClassification).map
+  return (
+    <div>
+      <p style={{ textAlign: "center" }}>
+        방 사진 열거 맨 sort by Classification Result
+      </p>
+      {Object.entries(showRoomsByClassification).map(([classifiedRoomType, roomData]) => (
+        <div>
+          <h1>{classifiedRoomType}</h1>
+          {/* <p>{roomData.img_path}</p> */}
+          {/* <p>{imageView(roomData.img_path)}</p> */}
+          <p>{Object.values(roomData.img_path).map((path)=> (
+            // <div><img src = {path}/></div>
+            <img src = {path}/>
+          ))}</p>
+          <h2>{JSON.stringify(roomData.detected)}</h2>
+          <p>확인</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Room = () => {
-    // 쿼리 
+    const navigate = useNavigate();
+    const [fieldErrors, setFieldErrors]=useState({});
+    // useSearchParams 사용
+    // setSearchParams의 경우 다음 페이지에 쿼리값을 전해주기 위해 사용 room?postid=3
+    const [searchParams, setSearchParams]=useSearchParams();
+    const data = {}
+    let userName = ''
+    let title = ''
+    let caption = ''
+    let thumbImgSrc = ''
+    let roomInfo = {}
+
+    // 쿼리 get으로 받아오기
+    const onFinish = values => {
+      async function fn() {
+          // const { username, password } = values;
+          const postid = searchParams.get('postid');
+
+          serverUrl =  `${serverUrl}?postid=${postid}`;
+          setFieldErrors({});
+
+          // const data = { username, password };
+          // GET요청
+          try{
+              await Axios.get(serverUrl)
+              .then(function (response) {
+                // 성공 핸들링
+                // db를 json 파일로 해서 받을 예정 response.data = json 형태
+                data = response.data.postInfo
+                userName = JSON.stringify(data.userName)
+                title = JSON.stringify(data.title)
+                thumbImgSrc = JSON.stringify(data.thumbImgSrc)
+                caption = JSON.stringify(data.caption)
+                
+                roomInfo = data.roomInfo
+
+                console.log(response);
+              })
+              .catch(function (error) {
+                // 에러 핸들링
+                
+                console.log(error);
+              })
+              .finally(function () {
+                // 항상 실행되는 영역
+                
+              });
+
+              // navigate("/accounts/login");
+
+          }catch(error){
+              if (error.response){
+                  notification.open({
+                      message: "방 정보 불러오기 실패",
+                      description: "죄송합니다.",
+                      icon: <FrownOutlined style={{ color: "#ff3333"}} />
+                  });
+
+                  const { data:filedsErrorMessages } = error.response;
+
+                  setFieldErrors(
+                      Object.entries(filedsErrorMessages).reduce(
+                          (acc, [fieldName, errors]) => {
+                              acc[fieldName] = {
+                                  validateStatus: "error",
+                                  help: errors.join(" ")
+                              };
+                              return acc;
+                          },
+                          {}
+                      )
+                  );
+              }
+          }
+      }
+      fn();
+  };
+    
     return (
       <div>
-        <h1 style={{textAlign:'center'}}>Room 페이지</h1>
-        <p style={{textAlign:'center'}}>Room 페이지 화면입니다</p>
         <hr></hr>
-        <h2 style={{textAlign:'left'}}>title</h2>
-        <src><p>이미지 n장</p></src>
-        {ImageExample()}
+        <h1 style={{textAlign:'left'}}>{title}</h1>
+        <p style={{textAlign:'right'}}>{userName}</p>
+        <img src = {thumbImgSrc} />
+        <p style={{ textAlign: "left" }}>{caption}</p>
 
-        <p>숙소 소개글...</p>
-        <p>detection 결과</p>
-        <p>classfication 결과</p>
-        <hr></hr>
+
+        <h2>상세 방 소개</h2>
+        
+        {othersImageView(roomInfo)}
       </div>
     );
   };
